547975170a6438b52e9787b52a89fa228ba72df0
tracer-oscpu2021 <tracer@oscpu.org>
2021-09-22 16:45:01 +0800

    linyouxu 20210238    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: 6ebe137fedb14fdda41cbcc062806d80            Boot ID: 42423b30aef84a779a4a44ca3f8d03eb     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-25-generic       Architecture: x86-64  16:45:01 up 2 days, 12 min,  1 user,  load average: 0.66, 0.52, 0.46

--- projects/cpu_axi_diff/vsrc/axi_rw.v
+++ projects/cpu_axi_diff/vsrc/axi_rw.v
@@ -217,7 +217,7 @@ module axi_rw # (
     wire overstep           = addr_end[3:ALIGNED_WIDTH] != 0;
 
     wire [7:0] axi_len      = aligned ? TRANS_LEN - 1 : {{7{1'b0}}, overstep};
-    wire [2:0] axi_size123     = AXI_SIZE[2:0];
+    wire [2:0] axi_size     = AXI_SIZE[2:0];
 
     wire [AXI_ADDR_WIDTH-1:0] axi_addr          = {rw_addr_i[AXI_ADDR_WIDTH-1:ALIGNED_WIDTH], {ALIGNED_WIDTH{1'b0}}};
     wire [OFFSET_WIDTH-1:0] aligned_offset_l    = {{OFFSET_WIDTH-ALIGNED_WIDTH{1'b0}}, {rw_addr_i[ALIGNED_WIDTH-1:0]}} << 3;
