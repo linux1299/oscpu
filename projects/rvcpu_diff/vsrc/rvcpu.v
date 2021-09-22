f4cfcba8b9d05a8cf9e209499fa616d234ad2859
tracer-oscpu2021 <tracer@oscpu.org>
2021-09-22 17:51:27 +0800

    linyouxu 20210238    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: 6ebe137fedb14fdda41cbcc062806d80            Boot ID: 42423b30aef84a779a4a44ca3f8d03eb     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-25-generic       Architecture: x86-64  17:51:27 up 2 days,  1:19,  1 user,  load average: 0.68, 0.34, 0.27

--- projects/rvcpu_diff/vsrc/rvcpu.v
+++ projects/rvcpu_diff/vsrc/rvcpu.v
@@ -1006,7 +1006,8 @@ reg [63:0] cycleCnt;
 reg [63:0] instrCnt;
 reg [63:0] regs_diff [0:31];
 
-
+reg [63:0] if_id_pc;
+reg [63:0] id_ex_pc;
 reg [63:0] ex_ls_pc;
 reg [63:0] ls_wb_pc;
 
@@ -1020,6 +1021,8 @@ reg ex_ls_inst_valid;
 reg ls_wb_inst_valid;
 
 always @(posedge clk) begin
+    if_id_pc <= o_ifu_addr;
+    id_ex_pc <= if_id_pc;
     ex_ls_pc <= id_ex_pc;
     ls_wb_pc <= ex_ls_pc;
 
