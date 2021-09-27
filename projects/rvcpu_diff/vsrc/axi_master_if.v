ba1f79c81f9f0d471c59be24f1ab14a3f92284f3
tracer-oscpu2021 <tracer@oscpu.org>
2021-09-27 21:16:17 +0800

    linyouxu 20210238    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: 6ebe137fedb14fdda41cbcc062806d80            Boot ID: 90802b3e62434a5d96b898b324b2b4bd     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-36-generic       Architecture: x86-64  21:16:17 up 11:43,  1 user,  load average: 0.69, 0.24, 0.13

--- projects/rvcpu_diff/vsrc/axi_master_if.v
+++ projects/rvcpu_diff/vsrc/axi_master_if.v
@@ -307,8 +307,8 @@ module axi_master_if # (
     // Write address channel signals
     assign axi_aw_id_o      = axi_id;
     
-    //assign axi_aw_addr_o    = axi_addr;
-    assign axi_aw_addr_o    = {rw_addr_i[63:3], 3'b0};
+    assign axi_aw_addr_o    = axi_addr;
+    //assign axi_aw_addr_o    = {rw_addr_i[63:3], 3'b0};
 
     assign axi_aw_len_o     = axi_len;
 
@@ -361,8 +361,8 @@ module axi_master_if # (
     // Read address channel signals
     assign axi_ar_valid_o   = r_state_addr;
 
-    //assign axi_ar_addr_o    = axi_addr;
-    assign axi_ar_addr_o    = {rw_addr_i[63:3], 3'b0};
+    assign axi_ar_addr_o    = axi_addr;
+    //assign axi_ar_addr_o    = {rw_addr_i[63:3], 3'b0};
 
     assign axi_ar_prot_o    = `AXI_PROT_UNPRIVILEGED_ACCESS;
     assign axi_ar_id_o      = axi_id;
