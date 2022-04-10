092017871ce0b42e86f5772414cf4672e06d1ef9
tracer-oscpu2021 <tracer@oscpu.org>
2022-04-10 23:04:18 +0800

    linyouxu 20210238    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: 6ebe137fedb14fdda41cbcc062806d80            Boot ID: 54a80e22b061442a8aedf724c7727826     Virtualization: vmware   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.13.0-39-generic       Architecture: x86-64  23:04:18 up  1:05,  1 user,  load average: 0.32, 0.31, 0.20

--- projects/rvcpu_diff/vsrc/axi_master_if.v
+++ projects/rvcpu_diff/vsrc/axi_master_if.v
@@ -365,7 +365,7 @@ module axi_master_if # (
     assign axi_ar_id_o      = axi_id;
     assign axi_ar_user_o    = axi_user;
     assign axi_ar_len_o     = axi_len;
-    assign axi_ar_size_o    = axi_size;
+    // assign axi_ar_size_o    = axi_size;
     assign axi_ar_size_o    = 3'b011;
 
     assign axi_ar_burst_o   = `AXI_BURST_TYPE_INCR;
