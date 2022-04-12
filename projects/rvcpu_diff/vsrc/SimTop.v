10993b71d86a53a1b371a2fe4d8121eb49816701
tracer-oscpu2021 <tracer@oscpu.org>
2022-04-12 19:48:09 +0800

    linyouxu 20210238    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: 6ebe137fedb14fdda41cbcc062806d80            Boot ID: 54a80e22b061442a8aedf724c7727826     Virtualization: vmware   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.13.0-39-generic       Architecture: x86-64  19:48:10 up 16:32,  1 user,  load average: 0.49, 0.29, 0.22

--- projects/rvcpu_diff/vsrc/SimTop.v
+++ projects/rvcpu_diff/vsrc/SimTop.v
@@ -22,7 +22,7 @@ wire ram_rw_wen_o  ;
 wire [63:0] ram_rw_addr_o ;
 wire [63:0] ram_rw_wdata_o;
 wire [2:0]  ram_rw_size_o ;
-wire [63:0]  ram_wmask = 64'hffff_ffff_ffff_ffff;
+wire [63:0]  ram_wmask = 64'hff;
 
 reg  [63:0] ram_rw_data_i;
 reg         ram_rw_ready_i;
