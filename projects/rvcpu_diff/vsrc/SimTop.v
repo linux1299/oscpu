ebf217c52670cecad14e9a8c4e204ba5dd333163
tracer-oscpu2021 <tracer@oscpu.org>
2022-04-14 10:31:24 +0800

    linyouxu 20210238    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: 6ebe137fedb14fdda41cbcc062806d80            Boot ID: 54a80e22b061442a8aedf724c7727826     Virtualization: vmware   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.13.0-39-generic       Architecture: x86-64  10:31:24 up 1 day,  2:04,  1 user,  load average: 0.88, 0.54, 0.46

--- projects/rvcpu_diff/vsrc/SimTop.v
+++ projects/rvcpu_diff/vsrc/SimTop.v
@@ -190,8 +190,8 @@ always @(posedge clock) begin
     skip <= 0;
   else if (ls_wb_inst==32'h7b)
     skip <= 1;
-  else if (instr_op_is_sys_r0)
-    skip <= 1;
+//   else if (instr_op_is_sys_r0)
+//     skip <= 1;
   else
     skip <= 0; 
 end
@@ -258,50 +258,50 @@ DifftestTrapEvent DifftestTrapEvent(
   .instrCnt           (instrCnt)
 );
 
-// DifftestCSRState DifftestCSRState(
-//   .clock              (clock),
-//   .coreid             (0),
-//   .priviledgeMode     (`RISCV_PRIV_MODE_M),
-//   .mstatus            (u_rvcpu.u_csr_file.mstatus),
-//   .sstatus            (0),
-//   .mepc               (u_rvcpu.u_csr_file.mepc),
-//   .sepc               (0),
-//   .mtval              (0),
-//   .stval              (0),
-//   .mtvec              (u_rvcpu.u_csr_file.mtvec),
-//   .stvec              (0),
-//   .mcause             (u_rvcpu.u_csr_file.mcause),
-//   .scause             (0),
-//   .satp               (0),
-//   .mip                (u_rvcpu.u_csr_file.mip),
-//   .mie                (u_rvcpu.u_csr_file.mie),
-//   .mscratch           (0),
-//   .sscratch           (0),
-//   .mideleg            (0),
-//   .medeleg            (0)
-// );
 DifftestCSRState DifftestCSRState(
   .clock              (clock),
   .coreid             (0),
   .priviledgeMode     (`RISCV_PRIV_MODE_M),
-  .mstatus            (0),
+  .mstatus            (u_rvcpu.u_csr_file.mstatus),
   .sstatus            (0),
-  .mepc               (0),
+  .mepc               (u_rvcpu.u_csr_file.mepc),
   .sepc               (0),
   .mtval              (0),
   .stval              (0),
-  .mtvec              (0),
+  .mtvec              (u_rvcpu.u_csr_file.mtvec),
   .stvec              (0),
-  .mcause             (0),
+  .mcause             (u_rvcpu.u_csr_file.mcause),
   .scause             (0),
   .satp               (0),
-  .mip                (0),
-  .mie                (0),
+  .mip                (u_rvcpu.u_csr_file.mip),
+  .mie                (u_rvcpu.u_csr_file.mie),
   .mscratch           (0),
   .sscratch           (0),
   .mideleg            (0),
   .medeleg            (0)
 );
+// DifftestCSRState DifftestCSRState(
+//   .clock              (clock),
+//   .coreid             (0),
+//   .priviledgeMode     (`RISCV_PRIV_MODE_M),
+//   .mstatus            (0),
+//   .sstatus            (0),
+//   .mepc               (0),
+//   .sepc               (0),
+//   .mtval              (0),
+//   .stval              (0),
+//   .mtvec              (0),
+//   .stvec              (0),
+//   .mcause             (0),
+//   .scause             (0),
+//   .satp               (0),
+//   .mip                (0),
+//   .mie                (0),
+//   .mscratch           (0),
+//   .sscratch           (0),
+//   .mideleg            (0),
+//   .medeleg            (0)
+// );
 
 DifftestArchFpRegState DifftestArchFpRegState(
   .clock              (clock),
