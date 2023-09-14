de83d969f921fb86c5f2a48b60e1293af227ff16
tracer-oscpu2021 <tracer@oscpu.org>
2023-09-14 20:53:00 +0800

    linyouxu 20210238    Static hostname: lin-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 621a0ba3d7054a65ab9e8c4bc9c8abe0            Boot ID: 7860ec1205a54bdb8d7c5252795d9374     Virtualization: vmware   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-76-generic       Architecture: x86-64  20:53:00 up 1 day, 10:54,  5 users,  load average: 0.93, 2.25, 2.32

--- projects/dand_riscv/vsrc/SimTop.v
+++ projects/dand_riscv/vsrc/SimTop.v
@@ -375,13 +375,13 @@ always @(posedge clock) begin
 end
 
 always@(*) begin
-  writeback_wen = u_DandRiscvSimple.writeback_RD_WEN & u_DandRiscvSimple.writeback_arbitration_isValid;
+  writeback_wen = u_DandRiscvSimple.writeback_RD_WEN & u_DandRiscvSimple.writeback_arbitration_isFiring;
   writeback_pc = u_DandRiscvSimple.writeback_PC[63:0];
   writeback_inst = u_DandRiscvSimple.writeback_INSTRUCTION[31:0];
 end
 
 always @(*) begin
-  if (u_DandRiscvSimple.writeback_arbitration_isValid)
+  if (u_DandRiscvSimple.writeback_arbitration_isFiring)
     writeback_valid = 1;
   else if (branch_ebreak_ecall_mret)
     writeback_valid = 1;
